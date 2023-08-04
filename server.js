68c1c60dbc40262fe82b108e51ab238911881750
emmaWuxy <emmamiao333@gmail.com>
2023-08-04 11:00:16 -0400

    add routing plan

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, ()=>{
+    console.log('app is running on port 3000');
+})
+
+app.get('/', (req, res) => {
+    res.send('homepage');
+})
+
+/*
+/ --> res = homepage
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+*/
