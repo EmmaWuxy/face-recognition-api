b5cb6c751dd68fcf4c58f9143d71a68fd808d742
emmaWuxy <emmamiao333@gmail.com>
2023-08-04 11:38:30 -0400

    Add /signin routing logic

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json())
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'emma',
+            email: 'emma@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
 
 app.listen(3000, ()=>{
     console.log('app is running on port 3000');
@@ -8,6 +22,16 @@ app.listen(3000, ()=>{
 
 app.get('/', (req, res) => {
     res.send('homepage');
+})
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password){
+            res.json('success');
+    } else {
+        res.status(400).json('error logging in');
+    }
+
 })
 
 /*
