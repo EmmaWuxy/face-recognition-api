73ccd145aec280544502897156da5372f7612880
emmaWuxy <emmamiao333@gmail.com>
2023-08-13 13:08:55 -0400

    wrap register operations into a transaction

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const cors = require('cors');
 const db = require('knex')({
     client: 'mysql',
     connection: {
-      host : '127.0.0.1',
-      port : 3306,
-      user : 'emmam',
-      password : 'wwj776677',
-      database : 'face-recognition'
+        host: '127.0.0.1',
+        port: 3306,
+        user: 'emmam',
+        password: 'wwj776677',
+        database: 'face-recognition'
     }
-  });
+});
 
 const app = express();
 app.use(express.json())
 app.use(cors());
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('app is running on port 3000');
 })
 
@@ -26,58 +26,75 @@ app.get('/', (req, res) => {
 
 app.post('/signin', (req, res) => {
     const { email, password } = req.body;
-    if (email === database.users[1].email && bcrypt.compareSync(password, database.users[1].password)){
-        res.json(database.users[1]);
-    }
-    else {
-        res.status(400).json('error logging in');
-    }   
+    // if (email === database.users[1].email && bcrypt.compareSync(password, database.users[1].password)){
+    //     res.json(database.users[1]);
+    // }
+    // else {
+    //     res.status(400).json('error logging in');
+    // } 
+
 
 })
 
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
-    bcrypt.hash(password, null, null, function(err, hash) {
+    var hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        // insert signin info
         db.insert({
-            name: name,
             email: email,
-            joined: new Date()
-        })
-        .into('users')
-        .then(userId => 
-            db('users')
-            .where('id', userId[0])
-            .select('name', 'email', 'entries', 'joined')
-            .then(user => res.json(user[0]))
-        )
-        .catch(err => res.status(400).json('unable to register'));
-    });
+            hash: hash
+            })
+            .into('login')
+            .transacting(trx)
+            .then(signinId => {
+                // insert users info
+                return db.insert({
+                    name: name,
+                    email: email,
+                    joined: new Date()
+                })
+                    .into(('users'))
+                    .transacting(trx)
+                    .then(userId => {
+                        // pass the registered user info back to frontend
+                        return db('users')
+                            .where('id', userId[0])
+                            .select('id','name', 'email', 'entries', 'joined')
+                            .transacting(trx);
+                    })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback);
+    })
+    .then(user => res.json(user[0]))
+    .catch(err => res.status(400).json('unable to register'));
 })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    db('users').where({id}).select('*')
+    db('users').where({ id }).select('*')
         .then(user => {
-            if (user.length){
+            if (user.length) {
                 res.json(user[0]);
             }
-            else{
+            else {
                 res.status(400).json('user not found');
             }
         })
         .catch(err => res.status(400).json('error getting user'));
-    
+
 })
 
 app.put('/image', (req, res) => {
-    const {id} = req.body;
-    db('users').where({id}).increment('entries', 1)
+    const { id } = req.body;
+    db('users').where({ id }).increment('entries', 1)
         .then(isSuccess => {
-            if (isSuccess){
-                db('users').where({id}).select('entries')
+            if (isSuccess) {
+                db('users').where({ id }).select('entries')
                     .then(entries => res.json(entries[0]))
             }
-            else{
+            else {
                 res.status(400).json('unable to get entries')
             }
         })
